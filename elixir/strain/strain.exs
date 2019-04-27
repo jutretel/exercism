0815505ebb267093202dafc28bc17bf5f1a8af83
Julia Tretel <juliatretel@gmail.com>
2019-04-26 22:39:10 -0300

    Update Strain problem - use comprehensions

--- elixir/strain/strain.exs
+++ elixir/strain/strain.exs
@@ -7,7 +7,7 @@ defmodule Strain do
   """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def keep(list, fun) do
-    Enum.reduce(list, [], &put_if(&1, &2, fun, true))
+    for item <- list, fun.(item), do: item
   end
 
   @doc """
@@ -18,15 +18,6 @@ defmodule Strain do
   """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def discard(list, fun) do
-    Enum.reduce(list, [], &put_if(&1, &2, fun, false))
-  end
-
-  @spec put_if(item :: any, list :: list(any), fun :: (any -> boolean), expected :: boolean) :: list(any)
-  defp put_if(item, list, fun, expected) do
-    if fun.(item) == expected do
-      list ++ [item]
-    else
-      list
-    end
+    for item <- list, !fun.(item), do: item
   end
 end
