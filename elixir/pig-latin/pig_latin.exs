2314e7247860fcdd3e8685fa37d72cef97131fd7
Julia Tretel <juliatretel@gmail.com>
2019-04-28 00:49:16 -0300

    Resolve Pig Latin problem

--- elixir/pig-latin/pig_latin.exs
+++ elixir/pig-latin/pig_latin.exs
@@ -1,4 +1,7 @@
 defmodule PigLatin do
+  @vowel_regex ~r{^[aeiou]|^y[^aeiou]|^x[^aeiou]}
+  @consonant_regex ~r{^([^aeiou]*qu|[^aeiou]+)*}
+
   @doc """
   Given a `phrase`, translate it a word at a time to Pig Latin.
 
@@ -15,5 +18,21 @@ defmodule PigLatin do
   """
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    phrase
+    |> String.split()
+    |> Enum.map(&translate_word/1)
+    |> Enum.join(" ")
+  end
+
+  defp translate_word(word) do
+    cond do
+      String.match?(word, @vowel_regex) -> word <> "ay"
+      true -> translate_consonant_word(word)
+    end
+  end
+
+  defp translate_consonant_word(word) do
+    [beginning] = Regex.run(@consonant_regex, word, capture: :first)
+    String.replace(word, beginning, "") <> beginning <> "ay"
   end
 end
